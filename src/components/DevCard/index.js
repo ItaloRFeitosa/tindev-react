c36132b8cc1b9f8472a6b3918e5c22287aba0a7b
ItaloRFeitosa <italo85199@gmail.com>
2020-03-06 12:06:56 -0300

    like-dislike ok

--- src/components/DevCard/index.js
+++ src/components/DevCard/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 import './styles.css';
 
-import like from '../../assets/like.svg'
-import dislike from '../../assets/dislike.svg'
+
 
 export default function DevCard(props){
-    const { avatar, name, bio } = props;
+    const { avatar, name, bio } = props.dev;
     return(
-        <div className='dev-card'>
             <div className='dev-card-info'>
                 <img src={avatar} alt={name}/>
                 <footer>
@@ -16,10 +14,6 @@ export default function DevCard(props){
                     <p>{bio}</p>
                 </footer>
             </div>
-            <div className='like-dislike'>
-                <button type='button'><img src={dislike} alt='dislike'/></button>
-                <button type='button'><img src={like} alt='like'/></button>
-            </div>
-        </div>
+            
     );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,11 +6,48 @@ import api from '../../services/api';
 
 import DevCard from '../../components/DevCard';
 
+import like from '../../assets/like.svg';
+import dislike from '../../assets/dislike.svg';
+
 import './styles.css';
 
 export default function Home({ match }){
 
     const [devs, setDevs] = useState([]);
+
+    async function likeButton(devId){
+        const res = await api.post(`/devs/${devId}/likes`, {},{
+            headers: {
+                user: match.params.devId,
+            }
+        });
+
+        console.log(res)
+        if (res.status == 200) {
+            setDevs(devs.filter( (dev) => dev._id !== devId));
+        } else {
+            console.log(res.data);
+        }
+        
+    }
+
+    async function dislikeButton(devId){
+       
+        const res = await api.post(`/devs/${devId}/dislikes`, {},{
+            headers: {
+                user: match.params.devId,
+            }
+        });
+
+        console.log(res)
+        if (res.status == 200) {
+            setDevs(devs.filter( (dev) => dev._id !== devId));
+        } else {
+            console.log(res.data);
+        }
+
+    }
+
 
     useEffect(() => {
         async function loadDevs(){
@@ -32,11 +69,12 @@ export default function Home({ match }){
         <header><img src={logo} alt='Tindev'/></header>
         <div className='home-container'>
             <ul>
-                    {devs.map(dev => <li><DevCard 
-                        avatar={dev.avatar} 
-                        name={dev.name}
-                        bio={dev.bio}
-                    /></li>)}
+                    {devs.map((dev) => <li key={dev._id}><DevCard dev={dev}/>
+                        <div className='like-dislike'>
+                            <button type='button' onClick={() => dislikeButton(dev._id)}><img src={dislike} alt='dislike'/></button>
+                            <button type='button' onClick={() => likeButton(dev._id)}><img src={like} alt='like'/></button>
+                        </div>   
+                    </li>)}
             </ul>
 
         </div>
